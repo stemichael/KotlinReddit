a9cd4450ec84896ea5032f1c16cafed0509ddbc4
stemichael <dmichaelst@gmail.com>
2017-07-12 15:09:24 +0100

    Added Extensions, RecycleView, Delegate

--- app/src/main/java/com/dmich/kotlinreddit/features/news/NewsFragment.kt
+++ app/src/main/java/com/dmich/kotlinreddit/features/news/NewsFragment.kt
@@ -1,3 +1,29 @@
 package com.dmich.kotlinreddit.features.news
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dmich.kotlinreddit.R
+import com.dmich.kotlinreddit.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
+class NewsFragment : Fragment() {
+
+    private val newsList by lazy {
+        news_list
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
+    }
+}
